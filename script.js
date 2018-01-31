f7f70970216000f6fe49fd23e8e1ab50bb4bb6d8
John Bellizzi <jabellizzi406@gmail.com>
2018-01-31 15:03:45 -0500

    Chapter 7 url input

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
-console.log("42" == 42);
-console.log("42" === 42);
+function goToWebsite() {
+  var url = document.getElementById('urlInput').value;
 
-var x = "banana" + 42 + 3.14;
-console.log(x);
-
-var y = "banana" + 42 * 2;
-console.log(y);
-
-var x = "3.14";
-console.log(parseFloat(x));
-console.log(parseInt(x));
-console.log(+x);
-
-if(42 === "42") {
-  console.log('equal and same type');
-}
-else if(42 == "42") {
-  console.log('equal but not same type');
-}
-else {
-  console.log('not equal');
+  window.open(url, '_blank');
 }
