2e77c34ee048a26e07a2eac542f34bb43482dd63
John Bellizzi <jabellizzi406@gmail.com>
2018-01-31 22:24:00 -0500

    Chapter 5 Activity 1

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
-console.log("42" == 42);
-console.log("42" === 42);
-
-var x = "banana" + 42 + 3.14;
-console.log(x);
-
-var y = "banana" + 42 * 2;
-console.log(y);
-
-var x = "3.14";
-console.log(parseFloat(x));
-console.log(parseInt(x));
-console.log(+x);
-
-if(42 === "42") {
-  console.log('equal and same type');
+function getUrl() {
+  return 'https://www.google.com';
 }
-else if(42 == "42") {
-  console.log('equal but not same type');
-}
-else {
-  console.log('not equal');
-}
+
+var urlToOpen = getUrl();
+
+window.open(urlToOpen, '_blank');
